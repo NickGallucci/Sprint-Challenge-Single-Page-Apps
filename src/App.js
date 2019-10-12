0db4e80c00cdd0d3139f04e0bbac1a83a94baee5
CryptoN999 <nicholasgallucci4@gmail.com>
2019-10-11 19:16:11 -0700

    finished MVP

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from "react";
 import Header from "./components/Header.js";
+import { Route, Link } from "react-router-dom";
+import styled from "styled-components";
+import CharacterList from "./components/CharacterList";
+import CharacterCard from "./components//CharacterCard";
+import WelcomePage from "./components/WelcomePage";
 
 
+//Styles
+const NavLinks = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const Links = styled(Link)`
+  text-decoration: none;
+  display: flex;
+  justify-content: space-around;
+  background: aquamarine;
+  width: 100%;
+  text-decoration: none;
+`;
+
+
+//App
 export default function App() {
   return (
     <main>
+      <NavLinks>
+        <Links>
+          <Link to="/">Home Page</Link>
+          <Link to="/CharacterList">Characters</Link>
+        </Links>
+      </NavLinks>
       <Header />
+
+      {/* Route */}
+      <Route exact path="/" component={Header} />
+      <Route exact path="/" component={WelcomePage} />
+      <Route exact path="/CharacterList" component={CharacterList} />
+      <Route path="/character-list/" component={CharacterCard} />
+      {/* <Route path="/LocationList" component={LocationList} /> */}
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,57 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import styled from "styled-components";
+import CharacterCard from "./CharacterCard";
+import SearchForm from "./SearchForm";
+
+// Styles
+const MainContainer = styled.div`
+  text-align: center;
+`;
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([]);
+  const [search, setSearch] = useState(" ");
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, []);
+    axios
+      .get(`https://rickandmortyapi.com/api/character/`)
+      .then(response => {
+        setCharacters(response.data.results);
+      })
+      .catch(error => {
+        console.log("Whoah", error);
+      });
+  }, [search]);
 
+  // Search
+  const handleChange = event => {
+    setSearch(event.target.value);
+  };
+
+  const filteredCharacters = () => {
+    characters.filter(character => {
+      character.name.toLowerCase().includes(search.toLowerCase());
+    });
+  };
   return (
-    <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
-    </section>
+    <MainContainer>
+      <section>
+        <h2>CHARACTERS</h2>
+        <SearchForm characters={characters} />
+        <SearchForm
+          placeholder="search characters"
+          handleChange={handleChange}
+        />
+        {characters.map(character => (
+          <CharacterCard
+            key={character.id}
+            name={character.name}
+            image={character.image}
+          />
+        ))}
+        <CharacterCard search={filteredCharacters} />
+      </section>
+    </MainContainer>
   );
 }
